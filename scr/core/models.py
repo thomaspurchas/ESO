5c5f24de3dc9ad5322dba1a80584f356ec9c60eb
Thomas Purchas <tpurchas@gmail.com>
2012-05-14 21:33:18 +0100

    Update core models
    The models file has been rearanged so that it works :). In addtion we now have lecuturer aliases for search reasons and also a username field for the lectureres.

--- scr/core/models.py
+++ scr/core/models.py
@@ -1,20 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class Document(models.model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(required=False)
-    author = models.ForeignKey(Lecturer)
-    module = models.ForeighKey(Module)
-
-    file = models.FileField(upload_to='document/%Y/%m/%d')
-
-class Lecturer(models.model):
+class Lecturer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     salutation = models.CharField(max_length=10)
+    username = models.CharField(max_length=10)
 
-class Module(models.model):
+class LecturerAliases(models.Model):
+    alias = models.CharField(max_length=100)
+    lecturer = models.ForeignKey(Lecturer)
+
+class Module(models.Model):
     short_code = models.CharField(max_length=10)
     title = models.CharField(max_length=200)
-    description = models.TextField(required=False)
+    description = models.TextField(blank=True)
+
+class Document(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    author = models.ForeignKey(Lecturer, null=True)
+    module = models.ForeignKey(Module, null=True)
+
+    file = models.FileField(upload_to='document/%Y/%m/%d')
