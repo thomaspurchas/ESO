62da2a96e3927a8fdd92c9a8ee9ece1313620ee1
Thomas Purchas <tpurchas@gmail.com>
2012-05-15 11:16:24 +0100

    Add new tempfile model and improve core models.
    I have added a new tempfile model to store and look after uploaded files while we wait for the user to provide us with more information.
    
    In addtion the core models have been in proved. In perticular we now store the md5sum of files so that we know when the same file has been uploaded twice.

--- scr/core/models.py
+++ scr/core/models.py
@@ -7,6 +7,12 @@ class Lecturer(models.Model):
     salutation = models.CharField(max_length=10)
     username = models.CharField(max_length=10)
 
+    def get_full_name():
+        return u'%s %s %s' % self.salutation, self.first_name, self.last_name
+
+    def __unicode__():
+        return get_full_name()
+
 class LecturerAliases(models.Model):
     alias = models.CharField(max_length=100)
     lecturer = models.ForeignKey(Lecturer)
@@ -16,10 +22,20 @@ class Module(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
 
+    def get_complete_title():
+        return u'%s - %s' % self.short_code, self.title
+
+    def __unicode__():
+        return self.get_complete_title()
+
 class Document(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     author = models.ForeignKey(Lecturer, null=True)
     module = models.ForeignKey(Module, null=True)
 
-    file = models.FileField(upload_to='document/%Y/%m/%d')
+    file = models.FileField(upload_to='documents/%Y/%m/%d')
+    md5_sum = models.CharField(max_length=64)
+
+    def get_absolute_url(self):
+        return "/document/%i/" % self.id
--- scr/upload/models.py
+++ scr/upload/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class TempFile(models.Model):
+    file = models.FileField(upload_to='upload-temp')
+    name = models.CharField(max_length=200)
+    upload_time = models.DateTimeField(auto_now_add=True)
+    md5_sum = models.CharField(max_length=64)
