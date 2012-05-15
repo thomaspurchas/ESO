cc662993c35c6970a6464daa7ddc2e63d7ab2c69
Thomas Purchas <tpurchas@gmail.com>
2012-05-15 11:18:20 +0100

    Add simple view to serve up stored files.

--- scr/core/views.py
+++ scr/core/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.views.static import serve
+from django.conf import settings
+
+from core.models import Document
+
+def serve_document(request, pk):
+    doc = Document.objects.get(pk=pk)
+
+    return serve(request, doc.file.name, settings.MEDIA_ROOT)
