1f67c8e1f695f799e3108466952d44ecb5f3ae83
Thomas Purchas <tpurchas@gmail.com>
2012-05-25 04:03:19 +0100

    Update search index to use either original file or derived pdf, whichever provides the most data.

--- scr/core/search_indexes.py
+++ scr/core/search_indexes.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 from haystack.indexes import *
 from haystack import site
-from core.models import Document
+from core.models import Document, DerivedFile
 
 
 class DocumentIndex(RealTimeSearchIndex):
@@ -19,12 +19,29 @@ class DocumentIndex(RealTimeSearchIndex):
         # Setup a solr instance for extract file contents
         solr = Solr(settings.SOLR_URL, timeout=20)
 
+        # Pass both the original and the PDF version to solr.
+        # Then commit whichever one returns the most raw data
+
         # Get the file
         raw_file = obj.file
 
-        extracted_data = solr.extract(raw_file)
+        file_extracted_data = solr.extract(raw_file)['contents']
 
-        data['text'] = extracted_data['contents']
+        # Get PDF
+        pdf_file_query = DerivedFile.objects.filter(pack__derived_from=obj)
+        pdf_file_query = pdf_file_query.filter(pack__type='pdf')
+
+        if len(pdf_file_query):
+            pdf_file = pdf_file_query[0].file
+            pdf_extracted_data = solr.extract(pdf_file)['contents']
+        else:
+            pdf_extracted_data = ''
+
+        if len(pdf_extracted_data) >  len(file_extracted_data):
+            data['text'] = pdf_extracted_data
+        else:
+            data['text'] = file_extracted_data
+
         return data
 
 site.register(Document, DocumentIndex)
