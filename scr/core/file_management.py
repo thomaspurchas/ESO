a8ff82237c3aa0b99f970f4ab65b845bd191b068
Thomas Purchas <tpurchas@gmail.com>
2012-05-27 16:58:34 +0100

    Add a bunch of file functions for handling md5 calculation, and also deleting files when their models are deleted.

--- scr/core/file_management.py
+++ scr/core/file_management.py
@@ -1,6 +1,17 @@
+import hashlib
 # A set of functions to be linked using signals
 
 def delete_file_on_model_delete(sender, instance, **kargs):
     file = getattr(instance, 'file', None)
     if file:
         file.delete()
+
+def generate_md5_sum(file_object):
+    if file_object:
+        md5 = hashlib.md5()
+        file_object.open('rb')
+        for chunk in file_object.chunks():
+            md5.update(chunk)
+        return md5.hexdigest()
+    return None
+
