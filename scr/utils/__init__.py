99a83a67d42153aabb1e01e0aaa14d3e78ced881
Thomas Purchas <tpurchas@gmail.com>
2012-05-27 17:03:00 +0100

    Add utils, with very useful get_object_or_none function.

---
+++ scr/utils/__init__.py
@@ -0,0 +1,6 @@
+# This file just contains useful functions
+def get_object_or_none(klass, *args, **kwargs):
+    try:
+        return klass._default_manager.get(*args, **kwargs)
+    except klass.DoesNotExist:
+        return None
