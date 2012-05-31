be8def443b39114b7e599f63dd35b6e1643b4872
Thomas Purchas <tpurchas@gmail.com>
2012-05-31 20:24:15 +0100

    Expand file name field max length

--- scr/upload/models.py
+++ scr/upload/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class TempFile(models.Model):
-    file = models.FileField(upload_to='upload-temp')
+    file = models.FileField(upload_to='upload-temp', max_length=200)
     name = models.CharField(max_length=200)
     upload_time = models.DateTimeField(auto_now_add=True)
     md5_sum = models.CharField(max_length=64)
